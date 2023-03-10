382e8845223a5c18e348787b0b257c6f3264eb60
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-03-10 09:31:48 +0530

    test

---
+++ js/common.js
@@ -0,0 +1,5 @@
+console.log("javascript is working 1");
+console.warn("javascript is working 2");
+console.info("javascript is working 3");
+console.error("javascript is working 4");
+console.debug("javascript is working 5");
