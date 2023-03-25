0f8504f9632205d0f2c3793b5dabfdd783db41b6
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-03-25 09:20:21 +0530

    event handling

--- js/myscript.js
+++ js/myscript.js
@@ -10,3 +10,7 @@ let html = function (id,message,isAdd=false) {
     }
 }
 let val = (id) => document.getElementById(id).value;
+//https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
+function isNumeric(value) {
+    return /^-?\d+$/.test(value);
+}
